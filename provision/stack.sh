87486adad8fc26c08060c4dac9a4c4b91c5a0928
Jonathan Fischoff <jfischoff@yahoo.com>
2017-06-11 19:39:49 -0400

    remove vagrant file. I'm not using it yet

--- provision/stack.sh
+++
@@ -1,7 +0,0 @@
-apt-get update -q
-apt-get install gcc libgmp-dev alex happy curl libpq-dev libcurl4-gnutls-dev libpcre3-dev libffi-dev make python-software-properties vim ctags git tmux ruby ufw fail2ban xz-utils zsh libpq-dev -y -q
-
-echo "-----> Installing Stack"
-wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | apt-key add -
-echo 'deb http://download.fpcomplete.com/ubuntu/trusty stable main'|tee /etc/apt/sources.list.d/fpco.list
-apt-get update && apt-get install stack -y
