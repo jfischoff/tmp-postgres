0e7f10e92c8fa08c7c621205b452190c0c6bb031
Jonathan Fischoff <jfischoff@yahoo.com>
2017-06-11 19:19:48 -0400

    more doc

--- src/Database/Postgres/Temp/Internal.hs
+++ src/Database/Postgres/Temp/Internal.hs
@@ -161,7 +161,7 @@ startWithLogger logger mainDir stdOut stdErr = try $ flip onException (rmDirIgno
     logger Finished
     return result
 
--- | Start postgres and log it's all stdout to TEMP_DIR/output.txt and TEMP_DIR/error.txt
+-- | Start postgres and log it's all stdout to '{'mainDir'}/output.txt' and '{'mainDir'}/error.txt'
 startAndLogToTmp :: IO (Either StartError DB)
 startAndLogToTmp = do
   mainDir <- createTempDirectory "/tmp" "tmp-postgres"
