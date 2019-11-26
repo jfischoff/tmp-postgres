6fd33d0f55cf9e200b85f68429f417fbb6022437
Jonathan Fischoff <jfischoff@yahoo.com>
2019-11-26 07:21:18 -0800

    expose CompleteDirectoryType publically

--- src/Database/Postgres/Temp/Config.hs
+++ src/Database/Postgres/Temp/Config.hs
@@ -59,6 +59,8 @@ module Database.Postgres.Temp.Config
   , keyBasedL
   -- ** 'DirectoryType'
   , DirectoryType (..)
+  -- ** 'CompleteDirectoryType
+  , CompleteDirectoryType (..)
   -- ** 'Accum'
   , Accum (..)
   -- ** 'Logger'
