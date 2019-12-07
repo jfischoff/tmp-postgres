1396495221ab0949dcd201da983a691f7572c90e
Jonathan Fischoff <jfischoff@users.noreply.github.com>
2019-12-07 08:36:17 -0800

    Fix compilation errors

--- profiling/Main.hs
+++ profiling/Main.hs
@@ -1,7 +1,5 @@
 import Database.Postgres.Temp
-import Database.Postgres.Temp.Internal.Core
 import Control.Exception
-import qualified Control.Monad as M
 
 withLoop :: IO ()
 withLoop = either throwIO pure =<< with (const $ pure ())
@@ -11,4 +9,4 @@ withCacheLoop = withDbCache $ \cache -> either throwIO pure =<<
   withConfig (defaultConfig <> cacheConfig cache) (const $ pure ())
 
 main :: IO ()
-main = timeIt "main" withCacheLoop
+main = withCacheLoop
