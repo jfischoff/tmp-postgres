7f2467a6d6d5f6db7eed59919a6773fe006cf22b
George Thomas <georgefsthomas@gmail.com>
2023-08-08 18:33:28 +0100

    Migrate to `prettyprinter` from deprecated `ansi-wl-pprint` (#279)
    We replace the `text` function in most places in favour of just using the already-enabled `OverloadedStrings`, or with `pretty` when not using string literals.
    
    Where the old instance `Pretty a => Pretty (Maybe a)` was relied upon, we replace `pretty` with `maybe mempty id` (then apply an HLint-suggested simplification), which is what we'd get from inlining a few definitions with the old library.

--- src/Database/Postgres/Temp/Internal/Core.hs
+++ src/Database/Postgres/Temp/Internal/Core.hs
@@ -13,9 +13,11 @@ import           Control.Monad
 import qualified Data.ByteString.Char8 as BSC
 import           Data.Foldable (for_)
 import           Data.IORef
+import           Data.Maybe
 import           Data.Typeable
 import qualified Database.PostgreSQL.Simple as PG
 import qualified Database.PostgreSQL.Simple.Options as Client
+import           Prettyprinter
 import           System.Directory
 import           System.Exit (ExitCode(..))
 import           System.IO
@@ -24,7 +26,6 @@ import           System.Posix.Signals (sigINT, sigQUIT, signalProcess)
 import           System.Process
 import           System.Process.Internals
 import           System.Timeout
-import           Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
 -- | Internal events for debugging
 --
@@ -152,32 +153,32 @@ data CompleteProcessConfig = CompleteProcessConfig
   -- ^ Whether or not to create new process group
   }
 
-prettyHandle :: Handle -> Doc
-prettyHandle _ = text "HANDLE"
+prettyHandle :: Handle -> Doc ann
+prettyHandle _ = "HANDLE"
 
-prettyKeyPair ::(Pretty a, Pretty b) => a -> b -> Doc
-prettyKeyPair k v = pretty k <> text ": " <> pretty v
+prettyKeyPair ::(Pretty a, Pretty b) => a -> b -> Doc ann
+prettyKeyPair k v = pretty k <> ": " <> pretty v
 
 instance Pretty CompleteProcessConfig where
   pretty CompleteProcessConfig {..}
-    =  text "completeProcessConfigEnvVars:"
+    =  "completeProcessConfigEnvVars:"
     <> softline
     <> indent 2 (vsep (map (uncurry prettyKeyPair) completeProcessConfigEnvVars))
     <> hardline
-    <> text "completeProcessConfigCmdLine:"
+    <> "completeProcessConfigCmdLine:"
     <> softline
-    <> text (unwords completeProcessConfigCmdLine)
+    <> pretty (unwords completeProcessConfigCmdLine)
     <> hardline
-    <> text "completeProcessConfigStdIn:"
+    <> "completeProcessConfigStdIn:"
     <+> prettyHandle completeProcessConfigStdIn
     <> hardline
-    <> text "completeProcessConfigStdOut:"
+    <> "completeProcessConfigStdOut:"
     <+> prettyHandle completeProcessConfigStdOut
     <> hardline
-    <> text "completeProcessConfigStdErr:"
+    <> "completeProcessConfigStdErr:"
     <+> prettyHandle completeProcessConfigStdErr
     <> hardline
-    <> text "completeProcessConfigCreateGroup:"
+    <> "completeProcessConfigCreateGroup:"
     <> softline
     <> pretty completeProcessConfigCreateGroup
 
@@ -240,15 +241,15 @@ data CompletePostgresPlan = CompletePostgresPlan
 
 instance Pretty CompletePostgresPlan where
   pretty CompletePostgresPlan {..}
-    =  text "completePostgresPlanProcessConfig:"
+    =  "completePostgresPlanProcessConfig:"
     <> softline
     <> indent 2 (pretty completePostgresPlanProcessConfig)
     <> hardline
-    <> text "completePostgresPlanClientOptions:"
+    <> "completePostgresPlanClientOptions:"
     <+> prettyOptions completePostgresPlanClientOptions
 
-prettyOptions :: Client.Options -> Doc
-prettyOptions = text . BSC.unpack . Client.toConnectionString
+prettyOptions :: Client.Options -> Doc ann
+prettyOptions = pretty . BSC.unpack . Client.toConnectionString
 
 -- | The output of calling 'startPostgresProcess'.
 data PostgresProcess = PostgresProcess
@@ -260,7 +261,7 @@ data PostgresProcess = PostgresProcess
 
 instance Pretty PostgresProcess where
   pretty PostgresProcess {..}
-    =   text "postgresProcessClientOptions:"
+    =   "postgresProcessClientOptions:"
     <+> prettyOptions postgresProcessClientOptions
 
 -- | Stop the @postgres@ process after attempting to terminate all the
@@ -327,15 +328,15 @@ data CompleteCopyDirectoryCommand = CompleteCopyDirectoryCommand
 
 instance Pretty CompleteCopyDirectoryCommand where
   pretty CompleteCopyDirectoryCommand {..}
-    =  text "copyDirectoryCommandSrc:"
+    =  "copyDirectoryCommandSrc:"
     <> softline
-    <> indent 2 (text copyDirectoryCommandSrc)
+    <> indent 2 (pretty copyDirectoryCommandSrc)
     <> hardline
-    <> text "copyDirectoryCommandDst:"
+    <> "copyDirectoryCommandDst:"
     <> softline
-    <> indent 2 (text copyDirectoryCommandDst)
+    <> indent 2 (pretty copyDirectoryCommandDst)
     <> hardline
-    <> text "copyDirectoryCommandCow:"
+    <> "copyDirectoryCommandCow:"
     <+> pretty copyDirectoryCommandCow
 
 executeCopyDirectoryCommand :: CompleteCopyDirectoryCommand -> IO ()
@@ -365,15 +366,15 @@ data InitDbCachePlan = InitDbCachePlan
 
 instance Pretty InitDbCachePlan where
   pretty InitDbCachePlan {..}
-    =   text "cachePlanDataDirectory:"
+    =   "cachePlanDataDirectory:"
     <>  softline
     <>  indent 2 (pretty cachePlanDataDirectory)
     <>  hardline
-    <>  text "cachePlanInitDb:"
+    <>  "cachePlanInitDb:"
     <>  softline
     <>  indent 2 (pretty cachePlanInitDb)
     <>  hardline
-    <>  text "cachePlanCopy:"
+    <>  "cachePlanCopy:"
     <>  softline
     <>  indent 2 (pretty cachePlanCopy)
 
@@ -413,32 +414,32 @@ data Plan = Plan
   , completePlanConnectionTimeout :: Int
   }
 
-eitherPretty :: (Pretty a, Pretty b) => Either a b -> Doc
+eitherPretty :: (Pretty a, Pretty b) => Either a b -> Doc ann
 eitherPretty = either pretty pretty
 
 instance Pretty Plan where
   pretty Plan {..}
-    =   text "completePlanInitDb:"
+    =   "completePlanInitDb:"
     <>  softline
-    <>  indent 2 (pretty $ fmap eitherPretty completePlanInitDb)
+    <>  indent 2 (fromMaybe mempty $ fmap eitherPretty completePlanInitDb)
     <>  hardline
-    <>  text "completePlanCopy:"
+    <>  "completePlanCopy:"
     <>  softline
     <>  indent 2 (pretty completePlanCopy)
     <>  hardline
-    <>  text "completePlanCreateDb:"
+    <>  "completePlanCreateDb:"
     <>  softline
     <>  indent 2 (pretty completePlanCreateDb)
     <>  hardline
-    <>  text "completePlanPostgres:"
+    <>  "completePlanPostgres:"
     <>  softline
     <>  indent 2 (pretty completePlanPostgres)
     <>  hardline
-    <>  text "completePlanConfig:"
+    <>  "completePlanConfig:"
     <>  softline
     <>  indent 2 (pretty completePlanConfig)
     <>  hardline
-    <>  text "completePlanDataDirectory:"
+    <>  "completePlanDataDirectory:"
     <+> pretty completePlanDataDirectory
 
 -- | 'startPlan' optionally calls @initdb@, optionally calls @createdb@ and
